49337171a132cca036df433313d8648a7c1754ee
Mahmud Milton <MahmudMilton@users.noreply.github.com>
2017-02-21 16:41:52 +0600

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        int j;
+        double e;
+        String r;
+
+        j = scan.nextInt();
+        e = scan.nextDouble();
+        scan.nextLine();
+        r = scan.nextLine();
+
+        System.out.println(j + i);
+        System.out.println(e + d);
+        System.out.println(s + r);
+
+        scan.close();
+    }
+}
